eb5046f3cd3b21d77f37c373bae37ea86c7ef1f6
Ayden Cook <cookayden2003@gmail.com>
2022-10-02 21:11:51 -0400

    added new example

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -5,6 +5,7 @@ pub mod particle;
 pub mod system;
 pub mod vec3;
 // optional feature: pub mod serialization
+// optional feature: parallelization or gpu acceleration
 
 pub mod prelude {
     pub use crate::{builtins::*, field::Field, particle::Particle, system::System, vec3::Vec3};
