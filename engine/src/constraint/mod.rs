90bf4b2e955e20090934b2e225fd201c3663cfdf
Ayden Cook <cookayden2003@gmail.com>
2022-11-29 15:33:56 -0500

    line separators

--- engine/src/constraint/mod.rs
+++ engine/src/constraint/mod.rs
@@ -7,3 +7,5 @@ use crate::particle::Particle;
 pub trait Constraint {
     fn project(&mut self, particle_source: &mut [Particle], dt: f64, static_pass: bool);
 }
+
+//---------------------------------------------------------------------------------------------------//
--- engine/src/interaction/mod.rs
+++ engine/src/interaction/mod.rs
@@ -10,3 +10,5 @@ use crate::particle::Particle;
 pub trait Interaction {
     fn handle(&mut self, particle_source: &mut [Particle], dt: f64);
 }
+
+//---------------------------------------------------------------------------------------------------//
